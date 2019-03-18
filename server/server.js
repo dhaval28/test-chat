66f30cade093a5889722185242a1bf999932dce6
Dhaval Petiwala <petiwaladhaval@gmail.com>
2019-03-18 20:42:08 +0530

    Added basic connect/disconnect events

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const path = require('path');
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
-console.log(publicPath);
+const port = process.env.PORT || 3000;
+
+const socketIO = require('socket.io');
+
+const express = require('express');
+var app = express();
+
+const http = require('http');
+var server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+
+    socket.on('disconnect', () => {
+        console.log('User dropped');
+    });
+});
 
 app.use(express.static(publicPath));
 
@@ -12,6 +26,6 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.post('/testPost', (req, res) => res.send('Post is working fine!'));
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Chat app listening on port ${port}!`);
 });
