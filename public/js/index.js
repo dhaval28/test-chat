d4936e48d4c5d388ee3c43af79e455921793273b
Dhaval Petiwala <petiwaladhaval@gmail.com>
2019-03-19 20:53:39 +0530

    Added welcome messages

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,14 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('receiveNewMessage', function (message) {
+socket.on('newMessage', function (message) {
     console.log('New message received', message);
+});
+
+socket.on('newUserConnected', function (message) {
+    console.log(message);
+});
+
+socket.on('welcomeMessage', function (message) {
+    console.log(message);
 });
--- server/server.js
+++ server/server.js
@@ -13,27 +13,41 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.on('sendNewMessage', function (message) {
+    socket.on('createMessage', function (message) {
         console.log("New message received from client to server", message);
-        io.emit('receiveNewMessage', {
+        //This emits to each and every client in the newtwork including itself
+        // io.emit('receiveNewMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        socket.broadcast.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });        
+    });
+
+    socket.broadcast.emit('newUserConnected', {
+        from: "Admin",
+        text: "New User Connected"
+    });
+
+    socket.emit('welcomeMessage', {
+        from: "Admin",
+        text: "Welcome to the Chat Room"
     });
 
     socket.on('disconnect', () => {
         console.log('User dropped');
     });
-    
 });
 
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => res.send('Hello World!'));
-
 app.post('/testPost', (req, res) => res.send('Post is working fine!'));
-
 
 server.listen(port, () => {
     console.log(`Chat app listening on port ${port}!`);
